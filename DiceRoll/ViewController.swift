e2b23d1a3fb57ff1444e44bae89ba5034b3474fa
Greg Willis <gregorymwillis@gmail.com>
2016-02-20 20:49:20 -0700

    Added new assests

--- DiceRoll/ViewController.swift
+++ DiceRoll/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var rollButton: UIButton!
+    @IBOutlet weak var firstDice: UIImageView!
+    @IBOutlet weak var secondDice: UIImageView!
+    @IBOutlet weak var diceRollLabel: UIImageView!
+    
 
+    var diceNameArray : [String] = ["oneDie", "twoDie", "threeDie", "fourDie", "fiveDie", "sixDie"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func clickedRollButton(sender: AnyObject) {
+        
+        let firstRandomNumber = Int(arc4random_uniform(6))
+        let firstDiceString:String = diceNameArray[firstRandomNumber]
+        self.firstDice.image = UIImage(named: firstDiceString)
+        
+        let secondRandomNumber = Int(arc4random_uniform(6))
+        let secondDiceString:String = diceNameArray[secondRandomNumber]
+        self.secondDice.image = UIImage(named: secondDiceString)
+
+    }
 
 }
 
